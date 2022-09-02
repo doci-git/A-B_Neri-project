6baa597925b6cd76456a1b14dc626dff246542d4
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-09-02 20:26:27 +0200

    Add files via upload
    first test mobile

---
+++ script.js
@@ -0,0 +1,178 @@
+  const toggleBtn = document.querySelector('.sidebar-toggle');
+  const closeBtn = document.querySelector('.close-btn');
+  const sidebar = document.querySelector('.sidebar');
+
+  toggleBtn.addEventListener('click', function () {
+    // if (sidebar.classList.contains("show-sidebar")) {
+    //   sidebar.classList.remove("show-sidebar");
+    // } else {
+    //   sidebar.classList.add("show-sidebar");
+    // }
+    sidebar.classList.toggle('show-sidebar');
+  });
+
+  closeBtn.addEventListener('click', function () {
+    sidebar.classList.remove('show-sidebar');
+  });
+
+  const menu = [{
+      id: 1,
+      title: "duomo",
+      category: "atrazioni",
+      img: "./fotofirenze/duomo.jpg",
+      desc: `La cattedrale metropolitana di Santa Maria del Fiore, conosciuta comunemente come duomo di Firenze, è la principale chiesa fiorentina, simbolo della città ed uno dei più famosi d'Italia; quando fu completata, nel Quattrocento, era la più grande chiesa al mondo, mentre oggi è ritenuta la terza in Europa dopo San Pietro a Roma e il Duomo di Milano. Essa sorge sulle fondamenta dell'antica cattedrale di Firenze, la chiesa di Santa Reparata, in un punto della città che ha ospitato edifici di culto sin dall'epoca romana. `,
+    },
+    {
+      id: 2,
+      title: "ponte vecchio",
+      category: "atrazioni",
+
+      img: "./fotofirenze/pontevechio.jpg",
+      desc: `Ponte vecchio, uno dei luoghi simbolo di Firenze, sorge in corrispondenza del ponte romano dell’antica Florentia. Nel corso dei secoli il ponte fu più volte ricostruito, anche a causa delle numerose piene dell’Arno; a seguito di una di queste (1333) si decise di costruirne uno nuovo, più stabile (con tre arcate sostenute da due soli piloni, con una serie di botteghe in muratura suentrambi i lati). Il ponte realizzato nel 1345, forse su progetto di Taddeo Gaddi, è sostanzialmente quello che ammiriamo tuttora. Le attuali botteghe di oreficeria  insediatesi nel 1593 al posto delle antiche macellerie e pescherie  ne fanno il ponte “più prezioso del mondo”. `,
+    },
+    {
+      id: 3,
+      title: "piazzale michelangelo",
+      category: "atrazioni",
+
+      img: "./fotofirenze/piazzale.jpg",
+      desc: `Fu realizzato dal 1869 su disegno dell'architetto Giuseppe Poggi su una collina appena a sud del centro storico, a completamento dei lavori di riqualificazione della riva sinistra dell'Arno. Da quell'anno infatti Firenze era capitale d'Italia e tutta la città era impegnata in un rinnovamento urbanistico, il cosiddetto Risanamento, ovvero la rinascita borghese della città: furono creati i lungarni; sulla riva destra, al posto delle mura trecentesche, furono aperti i viali di circonvallazione alla maniera dei boulevard; sulla riva sinistra fu tracciato, snodandosi sulla collina di San Miniato, il Viale dei Colli, una via panoramica alberata lunga 8 chilometri, al cui culmine fu realizzato il piazzale, quale terrazza panoramica privilegiata sulla città.`,
+    },
+    {
+      id: 4,
+      title: "galeria degli uffizi",
+      category: "atrazioni",
+
+      img: "./fotofirenze/galleria.jpg",
+      desc: `La Galleria degli Uffizi è un museo statale di Firenze, che fa parte del complesso museale denominato Gallerie degli Uffizi e comprendente, oltre alla suddetta galleria, il Corridoio vasariano, le collezioni di Palazzo Pitti e il Giardino di Boboli, che insieme costituiscono per quantità e qualità delle opere raccolte uno dei più importanti musei del mondo.
+
+      Il complesso appare nell'elenco redatto nel 1901 dalla Direzione Generale delle Antichità e Belle Arti, quale edificio monumentale da considerare patrimonio artistico nazionale.
+      
+      Vi si trovano la più cospicua collezione esistente di Raffaello e Botticelli, oltre a nuclei principali di opere di Giotto, Tiziano, Pontormo, Bronzino, Andrea del Sarto, Caravaggio, Dürer, Rubens, Leonardo da Vinci ed altri ancora. Mentre a Palazzo Pitti si concentrano le opere pittoriche del Cinquecento e del Barocco, ma anche dell'Otto e Novecento italiano, il corridoio vasariano ospitava fino al 2018 parte della collezione di autoritratti (oltre 1.700), che dovrebbero essere poi inclusi nel percorso espositivo della Galleria delle Statue e delle Pitture, come in piccola parte già avviene. `,
+    },
+    {
+      id: 5,
+      title: "giardini di boboli",
+      category: "atrazioni",
+
+      img: "./fotofirenze/boboli.jpg",
+      desc: `Il Giardino di Boboli è un parco storico della città di Firenze. Nato come giardino granducale di Palazzo Pitti, è connesso anche al Forte di Belvedere, avamposto militare per la sicurezza del sovrano e la sua famiglia. Il giardino, è uno dei più importanti esempi di giardino all'italiana al mondo ed è un vero e proprio museo all'aperto, per l'impostazione architettonico-paesaggistica e per la collezione di sculture, che vanno dalle antichità romane al XX secolo. Il giardino di Boboli è uno dei più famosi giardini della penisola.
+
+      I giardini furono costruiti tra il XVI e il XIX secolo, dai Medici, poi dagli Asburgo-Lorena e dai Savoia, e occupano un'area di circa 45.000 m². Alla prima impostazione di stile tardo-rinascimentale, visibile nel nucleo più vicino al palazzo, `,
+    },
+    {
+      id: 6,
+      title: "palazo pitti",
+      category: "atrazioni",
+
+      img: "./fotofirenze/pitti.jpg",
+      desc: `Palazzo Pitti è un imponente palazzo rinascimentale di Firenze. Si trova nella zona di Oltrarno, a breve distanza da Ponte Vecchio. Il nucleo originale dell'edificio risale al 1458, come residenza urbana del banchiere Luca Pitti. Il palazzo fu quindi acquistato dalla famiglia Medici nel 1549 e divenne la residenza principale dei granduchi di Toscana, prima Medici e dal 1737 Asburgo-Lorena. A seguito dell'unità d'Italia, svolse il ruolo di palazzo reale per Casa Savoia nel quinquennio in cui Firenze fu capitale del Regno d'Italia (1865-70). Nel 1919 Vittorio Emanuele III lo donò allo Stato: da allora è un museo statale.`,
+    },
+    {
+      id: 7,
+      title: "santa croce",
+      category: "atrazioni",
+
+      img: "./fotofirenze/santa-croce.jpg",
+      desc: `La basilica di Santa Croce nell'omonima piazza a Firenze, è una delle più grandi chiese francescane e una delle massime realizzazioni del gotico in Italia, e possiede il rango di basilica minore.
+
+      Santa Croce è un simbolo prestigioso di Firenze, il luogo di incontro dei più grandi artisti, teologi, religiosi, letterati, umanisti e politici, che determinarono, nella buona e cattiva sorte, l'identità della città tardo-medievale e rinascimentale. Al suo interno trovarono inoltre ospitalità celebri personaggi della storia della Chiesa come san Bonaventura, Pietro di Giovanni Olivi, sant'Antonio da Padova, san Bernardino da Siena, san Ludovico d'Angiò. Fu anche luogo d'accoglienza per pontefici come Sisto IV, Eugenio IV, Leone X, Clemente XIV.
+      
+      Il grande poeta neoclassico Ugo Foscolo la elesse nella sua opera Dei sepolcri a famedio nazionale d'Italia. Nella chiesa trovano infatti posto i sepolcri dei geni di più eccelsa raffinatezza che l'Italia abbia dato al mondo: le "urne dei forti", come Foscolo le chiama nel suo carme.`,
+    },
+    {
+      id: 8,
+      title: "museo nazionale di bargello",
+      category: "atrazioni",
+
+      img: "./fotofirenze/bargello.jpeg",
+      desc: `Con la costituzione di Firenze a libero comune e la creazione della figura del capitano del popolo, venne costruito il palazzo più tardi detto del Bargello. Il primo nucleo, affacciato su via del Proconsolo, già iniziato nel 1255, venne realizzato secondo Giorgio Vasari da Lapo Tedesco, la torre dei Boscoli e alcune case e torri della Badia Fiorentina, tra il 1340 e il 1345 l'edificio venne rialzato da Neri di Fioravante. Nel frattempo era diventato sede anche del podestà e del consiglio degli Anziani. Con l'instaurarsi dell'egemonia medicea nella seconda metà del Quattrocento, divenne prima la sede del Consiglio di Giustizia e dei Giudici di Ruota, e dal 1574, sotto il duca Cosimo I de' Medici, sede del Bargello, ovvero il capo delle Guardie o di Piazza, che provvedeva agli arresti, interrogatori e provvedeva anche ad eseguire le condanne capitali.
+
+      Nei quasi tre secoli, in cui venne adibito a carcere, nel cortile furono murati gli archi del loggiato e del verone, le sale più grandi vennero suddivise con tramezzi per ricavarne un maggior numero di celle e furono coperte le pitture e le decorazioni. `,
+    },
+    {
+      id: 9,
+      title: "museo galileo",
+      category: "atrazioni",
+
+      img: "./fotofirenze/galileo.jpg",
+      desc: `Il Museo Galileo raccoglie i preziosi strumenti scientifici provenienti dalle collezioni dei Medici e dei Lorena (secoli XVI-XIX).
+
+      L'origine della collezione medicea si deve a Cosimo I (1519-1574), che la collocò nella Guardaroba di Palazzo Vecchio (l'attuale Sala delle Carte Geografiche). Nel 1600 Ferdinando I (1549-1609) spostò gli strumenti nello Stanzino delle Matematiche della Galleria degli Uffizi. Nella terrazza attigua fu alloggiata la grande sfera armillare costruita da Antonio Santucci nel 1593. Nel corso del XVII secolo si aggiunsero gli strumenti destinati agli esperimenti dell'Accademia del Cimento (1657-1667), nella cui sede di Palazzo Pitti trovò successivamente posto tutta la raccolta medicea.`,
+    },
+    {
+      id: 10,
+      title: " Folon e il Giardino delle Rose",
+      category: "atrazioni",
+
+      img: "./fotofirenze/giardino-rose.jpg",
+      desc: `Giardino delle Rose fu realizzato nel 1865 dallo stesso architetto del piazzale, Giuseppe Poggi su incarico del Comune di Firenze in previsione dello spostamento della capitale d'Italia da Torino. Copre circa un ettaro di terreno terrazzato dal quale si gode una splendida vista panoramica della città, racchiuso fra l'attuale viale Poggi, via di San Salvatore, e via dei Bastioni.
+
+        Già appartenuto a una villetta di proprietà dei padri filippini e denominato "podere di San Francesco", venne poi spartito a terrazzamenti da Attilio Pucci che utilizzò la sua posizione e i muri di sostegno delle terrazze per dar vita ad una collezione di rose..`,
+    },
+  ];
+
+
+  const sectionCenter = document.querySelector('.section-center');
+  window.addEventListener('DOMContentLoadedì', function () {
+
+    let displayMenu = menu.map(function (item) {
+      // console.log(item);
+
+      return `<article class="menu-item">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          </article> <br>
+          <br><br> `;
+    });
+    displayMenu = displayMenu.join("");
+    // console.log(displayMenu);
+
+    sectionCenter.innerHTML = displayMenu;
+  });
+
+
+
+  // slider image apartments
+
+  const slides = document.querySelectorAll('.slide');
+  const nextBtn = document.querySelector('.nextBtn');
+  const prevtBtn = document.querySelector('.prevBtn');
+
+  slides.forEach(function (slide, index) {
+    slide.style.left = `${index * 100 }%`;
+
+  });
+  let counter = 0;
+
+  nextBtn.addEventListener('click', function () {
+    counter++;
+    carousel();
+  });
+
+  prevtBtn.addEventListener('click', function () {
+    counter--;
+    carousel()
+  });
+
+
+  function carousel() {
+    if (counter === slides.length) {
+      counter = 0;
+    }
+    if (counter < 0) {
+      counter = slides.length - 1;
+
+    }
+    slides.forEach(function (slide) {
+      slide.style.transform = `translateX(-${counter * 100}%)`
+    })
+  };
